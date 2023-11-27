75a1bab529885ac6476514d8d548889b4e01fdb7
Anderson Floriano <anderson.floriano@gmail.com>
2023-11-27 09:16:14 -0300

    Test

--- app/main/layout.js
+++ app/main/layout.js
@@ -1,10 +1,10 @@
-import Menu from "./components/Menu";
+// import Menu from "./components/Menu";
 
 export default function MainLayout({children}) {
   return (
     <div className="flex">
       <div className="h-screen w-1/4 min-w-[300px] bg-white flex justify-center pt-6">
-        <Menu />
+        {/* <Menu /> */}
       </div>
       <div className="h-screen w-3/4">{children}</div>
     </div>
